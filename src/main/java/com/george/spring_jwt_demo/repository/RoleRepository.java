941b3a4fc94343e57dd91e449a7fdeb2ff8cb886
Georg <gorgonady.gn@gmail.com>
2022-04-14 19:26:06 +0200

    first commit

--- src/main/java/com/george/spring_jwt_demo/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.george.spring_jwt_demo.repository;
-
-import com.george.spring_jwt_demo.domain.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByName(String name);
-}
